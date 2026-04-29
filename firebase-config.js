e145b477172ff3f1e8c0bc81f789e394567208f3
Maroshop <homworkhhh25@gmail.com>
2026-04-29 23:50:02 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyC1BqYs0H-8UvJeQ-kJn6q9LTq4-1dvjKM",
+    authDomain: "jumra-747da.firebaseapp.com",
+    databaseURL: "https://jumra-747da-default-rtdb.firebaseio.com",
+    projectId: "jumra-747da",
+    storageBucket: "jumra-747da.firebasestorage.app",
+    messagingSenderId: "46172184487",
+    appId: "1:46172184487:web:3bb6d17f135fb038df2082",
+    measurementId: "G-1QGD4FCQVZ"
+  },
+
+  firebaseRoot: "cashier_units_pro_jumra_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_jumra_885766842"
+  ]
+};
